401f0131b7a68cb60a0851113314ddbba2b349dd
adamkhamismohammedabdeljali <adamkhamismohammedabdeljalil@gmail.com>
2023-07-03 15:15:44 +0200

    feat:Add ExploreTheHashSet project

---
+++ ExploreTheHashSet/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExploreTheHashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a HashSet
+            HashSet<string> foods = new HashSet<string>();
+            //add unique values
+            foods.Add("pizza");
+            foods.Add("burger");
+            foods.Add("salad");
+            //Does a HashSet consider strings with different capitalisations unique?
+            foods.Add("Burger");
+            foods.Add("PIZZA");
+            Console.WriteLine(foods.Add("SALaD"));
+            // strings with different capitalizations are unique and could be added to hashset
+
+
+        }
+    }
+}
